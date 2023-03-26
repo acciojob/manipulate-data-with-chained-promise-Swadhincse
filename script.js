b6c69fb6127bfda0690f6b59a0fbc6479a3f7309
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-03-27 00:02:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function manipulateArray(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  })
+  .then(numbers => {
+    const evenNumbers = numbers.filter(num => num % 2 === 0);
+    return evenNumbers;
+  })
+  .then(evenNumbers => {
+    const doubledNumbers = evenNumbers.map(num => num * 2);
+    return doubledNumbers;
+  })
+  .then(finalNumbers => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = finalNumbers.join(", ");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
